ff4e9ec3db32837785a922aef09679f02776fe45
JuanRT <juan.m13@hotmail.fr>
2025-01-14 23:52:41 +0100

    changed daisyUI hero page layout

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,18 +1,23 @@
 
 const Hero = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hello there</h1>
+        <section id="hero" className="hero bg-base-100 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <img
+                    src="/juan-memoji-working.png"
+                    className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Salut c'est Juan !</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Je suis un développeur fullstack passionné par la technologie et le design. J'aime créer des applications web et mobiles qui ont un impact positif sur les gens.
+                        <br />
+                        <br />
+                        Je suis également un grand fan de la culture pop et des jeux vidéo. Si vous avez un projet ou une idée que vous aimeriez concrétiser, n'hésitez pas à me contacter.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-success hover:opacity-80">Télécharger mon CV</button>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 export default Hero;
