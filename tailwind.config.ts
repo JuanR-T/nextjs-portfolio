84bec89afe61804c6dbe6780b1751b5ae1358156
JuanRT <juan.m13@hotmail.fr>
2025-01-15 12:24:52 +0100

    Added tailwind-merge package and updated font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
             },
+            fontFamily: {
+                sans: "var(--font-sans)",
+                serif: "var(--font-serif)",
+            },
         },
     },
     plugins: [require("daisyui")],
