902b56bd2769d1964a3990e5f16eaa603576bf79
JuanRT <juan.m13@hotmail.fr>
2025-01-17 16:22:30 +0100

    Update footer and hero sections styling

--- src/Components/FooterSection/FooterSection.tsx
+++ src/Components/FooterSection/FooterSection.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <section id="footer" className="container">
-            <div className="w-full mt-16 md:mt-8 border-t-2 border-neutral py-10">
+            <div className="w-full mt-16 md:mt-0 border-t-2 border-neutral py-10">
                 <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full">
                     <p className="pt-4 text-sm md:text-lg md:pt-0">{currentYear} © All rights reserved</p>
                     <div className="flex justify-center items-center gap-x-10">
