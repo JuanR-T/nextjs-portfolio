af4aed349521efcfef5a689127980f3ea9597f03
JuanRT <juan.m13@hotmail.fr>
2025-01-17 15:59:17 +0100

    fixed footer responsive

--- src/Components/FooterSection/FooterSection.tsx
+++ src/Components/FooterSection/FooterSection.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <section id="footer" className="container">
-            <div className="w-full border-t-2 border-neutral py-10">
-                <div className="flex justify-between items-center w-full">
-                    <p>{currentYear} © All rights reserved</p>
+            <div className="w-full mt-16 md:mt-8 border-t-2 border-neutral py-10">
+                <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full">
+                    <p className="pt-4 text-sm md:text-lg md:pt-0">{currentYear} © All rights reserved</p>
                     <div className="flex justify-center items-center gap-x-10">
                         {contactData.map((contact, index) => {
                             return (
                                 <Link key={index} href={contact.link} className="tooltip" data-tip={contact.title} target="_blank">
-                                    {getTechIcon({ technology: contact.title, className: "text-2xl" })}
+                                    {getTechIcon({ technology: contact.title, className: "text-xl md:text-2xl" })}
                                 </Link>
                             )
                         })}
