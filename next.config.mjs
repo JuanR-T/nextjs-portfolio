63f75fb394e2f4598134fdcbff66df66d8d5e199
JuanRT <juan.m13@hotmail.fr>
2025-01-19 19:52:04 +0100

    Remove unused import and webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import path from "path";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
-    },
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            "@": path.join("./", "src"),
-        };
-        return config;
     },
 };
 
