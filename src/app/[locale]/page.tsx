856182eead612f3ec4696152c3d791a932a47785
JuanRT <juan.m13@hotmail.fr>
2025-01-19 18:23:48 +0100

    renamed navbar index file

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import ExperienceSection from "../../components/ExperienceSection/ExperienceSection.tsx";
 import Footer from "../../components/FooterSection/FooterSection.tsx";
 import Hero from "../../components/Hero/Hero.tsx";
-import Navbar from "../../components/Navbar/Navbar.tsx";
+import Navbar from "../../components/Navbar";
 import ProjectsSection from "../../components/ProjectsSection/ProjectsSection.tsx";
 import StackSection from "../../components/StackSection/StackSection.tsx";
 
