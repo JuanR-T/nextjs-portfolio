0a6781b916b29e84c0ae1d5c70f25fe2b257daf7
JuanRT <juan.m13@hotmail.fr>
2025-01-19 18:13:48 +0100

    Update imports to include file extensions

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,9 @@
-import ExperienceSection from "../../components/ExperienceSection/ExperienceSection";
-import Footer from "../../components/FooterSection/FooterSection";
-import Hero from "../../components/Hero/Hero";
-import Navbar from "../../components/Navbar/Navbar";
-import ProjectsSection from "../../components/ProjectsSection/ProjectsSection";
-import StackSection from "../../components/StackSection/StackSection";
+import ExperienceSection from "../../components/ExperienceSection/ExperienceSection.tsx";
+import Footer from "../../components/FooterSection/FooterSection.tsx";
+import Hero from "../../components/Hero/Hero.tsx";
+import Navbar from "../../components/Navbar/Navbar.tsx";
+import ProjectsSection from "../../components/ProjectsSection/ProjectsSection.tsx";
+import StackSection from "../../components/StackSection/StackSection.tsx";
 
 const Home = () => {
   {/** TODO: Check lazy loading components with next/dynamic */ }
